5ead3d0f1d305aef381b01933a6b2f7aa02c2562
Mriduldeep44 <eathan.mridul44@gmail.com>
2024-10-17 22:14:53 +0530

    setting up the logger and the exception file

---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,6 @@
+''' 
+we will import the files of the components as the modules 
+into the training_pipleline file and make use of it in training the dataset 
+that has alrady been ingested and transformed
+
+'''
