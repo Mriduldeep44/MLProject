31ef7258f66b5dcaf9d7f22ad6996c6e3411306c
Mriduldeep44 <eathan.mridul44@gmail.com>
2024-10-20 00:13:41 +0530

    data_ingestion code

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import src.components.exception import CustomException
+from src.components.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
