d14d96c750caa5a34634013ad9b37ff9a7119b3a
Mriduldeep44 <eathan.mridul44@gmail.com>
2024-10-21 15:40:58 +0530

    updated utils

--- src/components/utils.py
+++ src/components/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 from src.components.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
 #Dill is basically used for saving the object into the file by converting it into
 # first the pkl file
 #dill is also used in serialisation of complex python objects
@@ -16,5 +17,21 @@ def save_object(file_path,obj):# take the two parameters 1st being the location 
         with open(file_path,"wb") as file_obj: #opening the folder and dumping the object into the directory/folder
             dill.dump(obj,file_obj)
             
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+            
+        return report
     except Exception as e:
         raise CustomException(e,sys)
