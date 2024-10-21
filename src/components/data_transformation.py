a85f99fd3ea64428525b43cd19a9b9313dd6978b
Mriduldeep44 <eathan.mridul44@gmail.com>
2024-10-21 19:07:04 +0530

    made changes and removed error

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -45,7 +45,7 @@ class DataTransformation:
                 steps=[
                     ("imputer",SimpleImputer(strategy="most_frequent")),
                     ("One_hot_encoder",OneHotEncoder()),
-                    ("scaler",StandardScaler())
+                    ("scaler",StandardScaler(with_mean=False))
                 ]
             )
             logging.info(f"Categorical columns: {categorical_columns}")
@@ -85,7 +85,7 @@ class DataTransformation:
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.fit(input_feature_test_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
             
             train_arr=np.c_[
                 input_feature_train_arr,np.array(target_feature_train_df)
